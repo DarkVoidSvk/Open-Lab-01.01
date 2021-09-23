9df9fc34f7b7de5ed487cf90b2eee7cc55c981c2
DarkVoidSvk <samueljakubecsk@gmail.com>
2021-09-23 18:49:28 +0200

    Change

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -6,7 +6,20 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();
+
+            int intStr; bool intResultTryParse = int.TryParse(input, out intStr);
+            if (intResultTryParse == true && intStr < 10)
+            {
+
+                return true;
+
+            }
+            else 
+            {
+
+                return false;
+
+            }
         }
     }
 }
